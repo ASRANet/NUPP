5731651f7a0116e13d836a2d3a69f540b59b55bd
Nikolai <ivaylo.angelov@asranet.co.uk>
2016-07-28 14:02:59 +0100

    Added "Organisation" field to upload abstract page

--- NUPP/settings.py
+++ NUPP/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['NUPP_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'nupp.nuclearpowerplantconference.com', 'www.nuclearpowerplantconference.com', 'www.nupp.nuclearpowerplantconference.com']
 
